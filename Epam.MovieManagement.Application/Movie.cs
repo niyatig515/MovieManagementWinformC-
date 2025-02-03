52637bb906ccfc9af3a77df24203793738c8f002
Niyati <niyati_gupta@epam.com>
2025-02-03 18:29:36 +0530

    Initial commit

---
+++ Epam.MovieManagement.Application/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.MovieManagement.Application
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int ReleasedYear { get; set; }
+        public string Director { get; set; }
+
+        public Movie()
+        {
+        }
+        public Movie(int movieId, string title, string genre, int year, string director)
+        {
+            MovieId = movieId;
+            Title = title;
+            Genre = genre;
+            ReleasedYear = year;
+            Director = director;
+        }
+
+
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Epam.MovieManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMovieId = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblGenre = new System.Windows.Forms.Label();
+            this.lblReleaseYear = new System.Windows.Forms.Label();
+            this.lblDirector = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtGenre = new System.Windows.Forms.TextBox();
+            this.txtReleaseYear = new System.Windows.Forms.TextBox();
+            this.txtDirector = new System.Windows.Forms.TextBox();
+            this.TxtLoad = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnLoadAll = new System.Windows.Forms.Button();
+            this.txtMovieId = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblMovieId
+            // 
+            this.lblMovieId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMovieId.Location = new System.Drawing.Point(117, 64);
+            this.lblMovieId.Name = "lblMovieId";
+            this.lblMovieId.Size = new System.Drawing.Size(93, 42);
+            this.lblMovieId.TabIndex = 0;
+            this.lblMovieId.Text = "Movie Id";
+            this.lblMovieId.Click += new System.EventHandler(this.lblMovieId_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(117, 125);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(79, 34);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Title";
+            this.lblTitle.Click += new System.EventHandler(this.lblTitle_Click);
+            // 
+            // lblGenre
+            // 
+            this.lblGenre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGenre.Location = new System.Drawing.Point(117, 180);
+            this.lblGenre.Name = "lblGenre";
+            this.lblGenre.Size = new System.Drawing.Size(79, 41);
+            this.lblGenre.TabIndex = 2;
+            this.lblGenre.Text = "Genre";
+            this.lblGenre.Click += new System.EventHandler(this.lblGenre_Click);
+            // 
+            // lblReleaseYear
+            // 
+            this.lblReleaseYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReleaseYear.Location = new System.Drawing.Point(88, 236);
+            this.lblReleaseYear.Name = "lblReleaseYear";
+            this.lblReleaseYear.Size = new System.Drawing.Size(179, 42);
+            this.lblReleaseYear.TabIndex = 3;
+            this.lblReleaseYear.Text = "Release Year";
+            this.lblReleaseYear.Click += new System.EventHandler(this.lblReleaseYear_Click);
+            // 
+            // lblDirector
+            // 
+            this.lblDirector.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDirector.Location = new System.Drawing.Point(102, 299);
+            this.lblDirector.Name = "lblDirector";
+            this.lblDirector.Size = new System.Drawing.Size(108, 43);
+            this.lblDirector.TabIndex = 4;
+            this.lblDirector.Text = "Director";
+            this.lblDirector.Click += new System.EventHandler(this.lblDirector_Click);
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(273, 126);
+            this.txtTitle.Multiline = true;
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(124, 35);
+            this.txtTitle.TabIndex = 6;
+            this.txtTitle.TextChanged += new System.EventHandler(this.txtTitle_TextChanged);
+            // 
+            // txtGenre
+            // 
+            this.txtGenre.Location = new System.Drawing.Point(273, 180);
+            this.txtGenre.Multiline = true;
+            this.txtGenre.Name = "txtGenre";
+            this.txtGenre.Size = new System.Drawing.Size(124, 33);
+            this.txtGenre.TabIndex = 7;
+            this.txtGenre.TextChanged += new System.EventHandler(this.txtGenre_TextChanged);
+            // 
+            // txtReleaseYear
+            // 
+            this.txtReleaseYear.Location = new System.Drawing.Point(273, 236);
+            this.txtReleaseYear.Multiline = true;
+            this.txtReleaseYear.Name = "txtReleaseYear";
+            this.txtReleaseYear.Size = new System.Drawing.Size(124, 42);
+            this.txtReleaseYear.TabIndex = 8;
+            this.txtReleaseYear.TextChanged += new System.EventHandler(this.txtRelease_TextChanged);
+            // 
+            // txtDirector
+            // 
+            this.txtDirector.Location = new System.Drawing.Point(273, 299);
+            this.txtDirector.Multiline = true;
+            this.txtDirector.Name = "txtDirector";
+            this.txtDirector.Size = new System.Drawing.Size(124, 35);
+            this.txtDirector.TabIndex = 9;
+            this.txtDirector.TextChanged += new System.EventHandler(this.txtDirector_TextChanged);
+            // 
+            // TxtLoad
+            // 
+            this.TxtLoad.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.TxtLoad.Location = new System.Drawing.Point(665, 65);
+            this.TxtLoad.Multiline = true;
+            this.TxtLoad.Name = "TxtLoad";
+            this.TxtLoad.ReadOnly = true;
+            this.TxtLoad.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TxtLoad.Size = new System.Drawing.Size(284, 269);
+            this.TxtLoad.TabIndex = 10;
+            this.TxtLoad.TextChanged += new System.EventHandler(this.TxtLoad_TextChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(78, 389);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(89, 47);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(189, 389);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(111, 47);
+            this.btnUpdate.TabIndex = 12;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(342, 389);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(104, 47);
+            this.btnDelete.TabIndex = 13;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFirst.Location = new System.Drawing.Point(509, 147);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(106, 43);
+            this.btnFirst.TabIndex = 14;
+            this.btnFirst.Text = "First";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLast.Location = new System.Drawing.Point(509, 236);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(106, 50);
+            this.btnLast.TabIndex = 15;
+            this.btnLast.Text = "Last";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(823, 392);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(126, 40);
+            this.btnClear.TabIndex = 17;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnLoadAll
+            // 
+            this.btnLoadAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoadAll.Location = new System.Drawing.Point(665, 389);
+            this.btnLoadAll.Name = "btnLoadAll";
+            this.btnLoadAll.Size = new System.Drawing.Size(116, 47);
+            this.btnLoadAll.TabIndex = 18;
+            this.btnLoadAll.Text = "Load All";
+            this.btnLoadAll.UseVisualStyleBackColor = true;
+            this.btnLoadAll.Click += new System.EventHandler(this.btnLoadAll_Click);
+            // 
+            // txtMovieId
+            // 
+            this.txtMovieId.Location = new System.Drawing.Point(273, 48);
+            this.txtMovieId.Multiline = true;
+            this.txtMovieId.Name = "txtMovieId";
+            this.txtMovieId.Size = new System.Drawing.Size(124, 41);
+            this.txtMovieId.TabIndex = 19;
+            this.txtMovieId.TextChanged += new System.EventHandler(this.txtMovieId_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Linen;
+            this.ClientSize = new System.Drawing.Size(1022, 541);
+            this.Controls.Add(this.txtMovieId);
+            this.Controls.Add(this.btnLoadAll);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.btnFirst);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.TxtLoad);
+            this.Controls.Add(this.txtDirector);
+            this.Controls.Add(this.txtReleaseYear);
+            this.Controls.Add(this.txtGenre);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.lblDirector);
+            this.Controls.Add(this.lblReleaseYear);
+            this.Controls.Add(this.lblGenre);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblMovieId);
+            this.Location = new System.Drawing.Point(100, 100);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMovieId;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblGenre;
+        private System.Windows.Forms.Label lblReleaseYear;
+        private System.Windows.Forms.Label lblDirector;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtGenre;
+        private System.Windows.Forms.TextBox txtReleaseYear;
+        private System.Windows.Forms.TextBox txtDirector;
+        private System.Windows.Forms.TextBox TxtLoad;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnLoadAll;
+        private System.Windows.Forms.TextBox txtMovieId;
+    }
+}
+
